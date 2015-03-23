9a1fa7388a5d2f746c1835aa69ae40ece11b03fa
liorg <lior.grosman@gmail.com>
2015-03-23 12:53:39 +0200

    gg

---
+++ Web/sln/sln/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(sln.Startup))]
+namespace sln
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
