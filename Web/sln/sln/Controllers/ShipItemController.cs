65466decd1dfcdb35c540687d8e849298c25c14d
lior grossman <lior_g@g-s.co.il>
2015-07-23 01:11:14 +0300

    sdsa

--- Web/sln/sln/Controllers/ShipItemController.cs
+++ Web/sln/sln/Controllers/ShipItemController.cs
@@ -55,8 +55,8 @@ namespace sln.Controllers
             {
                 ViewBag.ShipId = id;
                 Guid shipId = Guid.Parse(id);
-                var shippingItems = await context.ShippingItem.Where(s => s.IsActive == true && s.Shipping_ShippingId == shipId && s.Product!=null && s.Product.IsCalculatingShippingInclusive==false).ToListAsync();
-               
+                var shippingItems = await context.ShippingItem.Where(s => s.IsActive == true && s.Shipping_ShippingId == shipId && s.Product != null && s.Product.IsCalculatingShippingInclusive == false).ToListAsync();
+
                 var model = new List<ShippingItemVm>();
                 foreach (var shipItem in shippingItems)
                 {
@@ -64,7 +64,6 @@ namespace sln.Controllers
                     var u = new ShippingItemVm();
                     u.Id = shipItem.ShippingItemId;
                     u.ProductName = shipItem.Product != null ? shipItem.Product.Name : "";
-                    //u.Total=shipItem.
                     u.Name = shipItem.Name;
                     u.Total = Convert.ToInt32(shipItem.Quantity);
                     model.Add(u);
@@ -96,15 +95,15 @@ namespace sln.Controllers
             using (var context = new ApplicationDbContext())
             {
                 var shippingItem = new ShippingItem();
-                 Guid userid = Guid.Empty;
+                Guid userid = Guid.Empty;
 
                 ClaimsIdentity claimsIdentity = AuthenticationManager.User.Identity as ClaimsIdentity;
-                 foreach (var claim in claimsIdentity.Claims)
-                 {
-                     if (claim.Type == ClaimTypes.NameIdentifier)
-                         userid = Guid.Parse(claim.Value);
+                foreach (var claim in claimsIdentity.Claims)
+                {
+                    if (claim.Type == ClaimTypes.NameIdentifier)
+                        userid = Guid.Parse(claim.Value);
 
-                 }
+                }
 
                 shippingItem.ShippingItemId = Guid.NewGuid();
                 shippingItem.Shipping_ShippingId = shippingItemVm.ShipId;
@@ -130,17 +129,22 @@ namespace sln.Controllers
                 model.ShipId = Guid.Parse(id);
 
                 var shipItem = await context.ShippingItem.FindAsync(model.ShipId);
-                if (shipItem == null || shipItem.Shipping==null)
+                if (shipItem == null || shipItem.Shipping == null)
                     throw new ArgumentNullException("shipItem");
-               var ship= shipItem.Shipping;
-               var org = ship.Organization_OrgId;
-               var products = await context.Product.Where(s => s.Organizations.Any(e => org.HasValue && e.OrgId == org.Value)).ToListAsync();
-               model.OrderNumber = ship.Name;
-                    ViewBag.Products = new SelectList(products, "ProductId", "Name");
-                    ViewBag.ShipId = ship.ShippingId;
-                    ViewBag.OrderNumber = model.OrderNumber;
-                    return View(model);
-                
+                var ship = shipItem.Shipping;
+                var org = ship.Organization_OrgId;
+                var products = await context.Product.Where(s => s.Organizations.Any(e => org.HasValue && e.OrgId == org.Value)).ToListAsync();
+                model.OrderNumber = ship.Name;
+                model.Id = shipItem.ShippingItemId;
+                model.ProductId = shipItem.Product_ProductId.GetValueOrDefault();
+                model.Total = Convert.ToInt32(shipItem.Quantity);
+
+                ViewBag.Products = new SelectList(products, "ProductId", "Name");
+                ViewBag.ShipId = ship.ShippingId;
+                ViewBag.OrderNumber = model.OrderNumber;
+
+                return View(model);
+
             }
         }
 
@@ -151,7 +155,7 @@ namespace sln.Controllers
             {
                 Guid userid = Guid.Empty;
                 var shippingItem = await context.ShippingItem.FindAsync(shippingItemVm.Id);
-                
+
                 ClaimsIdentity claimsIdentity = AuthenticationManager.User.Identity as ClaimsIdentity;
                 foreach (var claim in claimsIdentity.Claims)
                 {
@@ -159,14 +163,14 @@ namespace sln.Controllers
                         userid = Guid.Parse(claim.Value);
                 }
 
-                shippingItem.Shipping_ShippingId = shippingItemVm.ShipId;
                 shippingItem.Quantity = shippingItemVm.Total;
-                
+
                 shippingItem.ModifiedOn = DateTime.Now;
                 shippingItem.ModifiedBy = userid;
 
+                context.Entry<ShippingItem>(shippingItem).State = EntityState.Modified;
                 await context.SaveChangesAsync();
-                return RedirectToAction("Index", new { id = shippingItemVm.ShipId.ToString() });
+                return RedirectToAction("Index", new { id = shippingItem.Shipping_ShippingId.Value.ToString() });
             }
         }
 
