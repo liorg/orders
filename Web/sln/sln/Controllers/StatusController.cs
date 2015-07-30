a5d891423f06c9fe649b54f9ad68aa9acf480f32
lior grossman <lior_g@g-s.co.il>
2015-07-30 10:49:06 +0300

    asa

--- Web/sln/sln/Controllers/StatusController.cs
+++ Web/sln/sln/Controllers/StatusController.cs
@@ -57,11 +57,11 @@ namespace sln.Controllers
                 context.Entry<Shipping>(ship).State = EntityState.Modified;
                 await context.SaveChangesAsync();
 
-                return RedirectToAction("Index","S");
+                return RedirectToAction("Index", "S");
             }
         }
 
-        public async Task<ActionResult> ConfirmRequest(string id,string assignTo)
+        public async Task<ActionResult> ConfirmRequest(string id, string assignTo)
         {
             using (var context = new ApplicationDbContext())
             {
@@ -71,7 +71,9 @@ namespace sln.Controllers
                 Guid shipId = Guid.Parse(id);
                 var ship = await context.Shipping.FindAsync(shipId);
                 Guid approval = Guid.Parse(Helper.Status.Confirm);
+                MemeryCacheDataService cache = new MemeryCacheDataService();
 
+                var grantToText = "";
                 var currentDate = DateTime.Now;
                 if (ship != null)
                 {
@@ -83,15 +85,18 @@ namespace sln.Controllers
                     if (String.IsNullOrEmpty(assignTo))
                     {
                         ship.GrantRunner = Guid.Parse(assignTo);
+                        grantToText = cache.GetRunners(context).Where(run => run.Id == assignTo).Select(run2=>run2.FullName).FirstOrDefault(); 
+
                     }
                     else
                     {
                         ship.GrantRunner = userid;
+                        grantToText = user.FullName;
                     }
                     TimeLine tl = new TimeLine
                     {
                         Name = "הזמנה אושרה ע'' חברת השליחות" + "של " + user.FullName + " (" + user.EmpId + ")",
-                        Desc = "הזמנה אושרה " + " " + ship.Name + " " + "בתאריך " + currentDate.ToString("dd/MM/yyyy hh:mm"),
+                        Desc = "הזמנה אושרה " + " " + ship.Name + " " + "בתאריך " + currentDate.ToString("dd/MM/yyyy hh:mm") +" והועברה לשליח"+ " "+grantToText,
                         CreatedBy = userid,
                         CreatedOn = currentDate,
                         ModifiedBy = userid,
