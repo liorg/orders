05d9588328f0c8c4f97004fd54cd4710288810d3
lior grossman <lior_g@g-s.co.il>
2015-08-03 18:15:45 +0300

    ssadas

---
+++ Web/sln/sln/Models/ViewItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace sln.Models
+{
+    public class ViewItem
+    {
+        public int StatusId { get; set; }
+        public string StatusDesc { get; set; }
+
+        public Func<sln.DataModel.Shipping, sln.Contract.IUserContext, bool> GetOnlyMyRecords { get; set; }
+
+        public Func<sln.Contract.IUserContext, bool> GetDefaultView { get; set; }
+
+        //public bool IsDefaultAdmin { get; set; }
+        //public bool IsDefaultOrgManager { get; set; }
+        //public bool IsDefaultAccept { get; set; }
+        //public bool IsDefaultUser { get; set; }
+        //public bool IsDefaultRunner { get; set; }
+
+        //public bool IsVisbleForAdmin { get; set; }
+        //public bool IsVisbleForOrgManager { get; set; }
+        //public bool IsVisbleForAccept { get; set; }
+        //public bool IsVisbleForUser { get; set; }
+        //public bool IsVisbleForRunner { get; set; }
+    }
+}
