81311e8551de11f68a0179927f7ba32d1c69db23
lior grossman <lior_g@g-s.co.il>
2015-08-30 14:22:07 +0300

    ggg

--- Web/sln/UTConsole/Program.cs
+++ Web/sln/UTConsole/Program.cs
@@ -7,6 +7,7 @@ using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
 
@@ -16,22 +17,122 @@ namespace UTConsole
     {
         static void Main(string[] args)
         {
-            Init();
+            Console.WriteLine("1.0.0.3");
+           // Merge();
+            var file = System.Configuration.ConfigurationSettings.AppSettings["file"].ToString();
+
+            Init(file);
+            //
+
+            //Test(@"rechovArrange2015-08-30 1127.json");
+            //Test(@"rechovArrange2015-08-30 1228.json");
+            //Test(@"rechovArrange2.json");
+        }
+
+        static void Test(string f)
+        {
+            StreetsGeoLocation location = new StreetsGeoLocation();
+            location.StreetsItems = new List<StreetLatAndLng>();
+          
+            using (StreamReader file = File.OpenText(f))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                StreetsGeoLocation locationDes = (StreetsGeoLocation)serializer.Deserialize(file, typeof(StreetsGeoLocation));
+                var items = locationDes.StreetsItems.Where(w => String.IsNullOrEmpty(w.Status) || w.Status == "new" || w.Status == "OVER_QUERY_LIMIT").OrderBy(d => d.UId).ToList();
+                var all = items.Count;
+                Console.WriteLine("file={0}, count={1}",f, all);
+            }
 
         }
-        static void Init()
+
+        static void Merge()
         {
             StreetsGeoLocation location = new StreetsGeoLocation();
             location.StreetsItems = new List<StreetLatAndLng>();
             int idx = 0;
-            using (StreamReader file = File.OpenText(@"rechovArrange.json"))
+            using (StreamReader rechovArrange = File.OpenText(@"rechovArrange.json"))
+            using (StreamReader file = File.OpenText(@"rechovArrange2015-08-30 1028.json"))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                StreetsGeoLocation locationTarget = (StreetsGeoLocation)serializer.Deserialize(file, typeof(StreetsGeoLocation));
+                StreetsGeoLocation locationSource = (StreetsGeoLocation)serializer.Deserialize(rechovArrange, typeof(StreetsGeoLocation));
+                
+                var itemsTarget = locationTarget.StreetsItems.OrderBy(d => d.Id).ToList();
+                var itemsSource= locationSource.StreetsItems.OrderBy(d => d.Id).ToList();
+
+
+                var all = itemsSource.Count;
+                foreach (var streetSource in itemsSource)
+                {
+                    idx++;
+                    var streetLatAndLng = new StreetLatAndLng();
+                    streetLatAndLng.UId = idx;
+                    streetLatAndLng.Addr = streetSource.Addr;
+                    streetLatAndLng.City = streetSource.City;
+                    streetLatAndLng.CodeAddr = streetSource.CodeAddr;
+                    streetLatAndLng.CodeCity = streetSource.CodeCity;
+                    streetLatAndLng.Id = streetSource.Id;
+                    streetLatAndLng.Tbl = streetSource.Tbl;
+                    streetLatAndLng.GoogleApiUrl = streetSource.GoogleApiUrl;
+
+                    streetLatAndLng.Lat = streetSource.Lat;
+                    streetLatAndLng.Lng = streetSource.Lng;
+                    Console.WriteLine("item {0} of {1}", idx, all);
+
+
+                    foreach (var streetTarget in itemsTarget)
+                    {
+                        if (streetTarget.UId == streetLatAndLng.UId)
+                        {
+                            streetLatAndLng.Lat = streetTarget.Lat;
+                            streetLatAndLng.Lng = streetTarget.Lng;
+                            streetLatAndLng.Status = streetTarget.Status;
+                            break;
+                        }
+                    }
+                    if (streetLatAndLng.Lat == 0.0 && String.IsNullOrEmpty(streetLatAndLng.Status))
+                    {
+                        streetLatAndLng.Status = "new";
+                    }
+                    else  if (streetLatAndLng.Lat != 0.0 && String.IsNullOrEmpty(streetLatAndLng.Status))
+                    
+                    {
+                        streetLatAndLng.Status = "ok";
+                    }
+                   
+                    location.StreetsItems.Add(streetLatAndLng);
+
+                }
+            }
+            using (FileStream fs = File.Open(@"rechovArrange2.json", FileMode.OpenOrCreate))
+            using (StreamWriter sw = new StreamWriter(fs))
+            using (JsonWriter jw = new JsonTextWriter(sw))
+            {
+                jw.Formatting = Formatting.Indented;
+
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.Serialize(jw, location);
+            }
+
+
+        }
+
+        static void Init(string fileJosn)
+        {
+            StreetsGeoLocation location = new StreetsGeoLocation();
+            location.StreetsItems = new List<StreetLatAndLng>();
+            int idx = 0;
+            using (StreamReader file = File.OpenText(fileJosn))
+           
+          //  using (StreamReader file = File.OpenText(@"rechovArrange2.json"))
             {
                 JsonSerializer serializer = new JsonSerializer();
                 StreetsGeoLocation locationDes = (StreetsGeoLocation)serializer.Deserialize(file, typeof(StreetsGeoLocation));
-                var items = locationDes.StreetsItems.Where(w => w.Status == "new" || w.Status == "OVER_QUERY_LIMIT").OrderBy(d => d.Id).ToList();
-                var all = items.Count;
+                var items = locationDes.StreetsItems.Where(w => String.IsNullOrEmpty( w.Status) ||  w.Status == "new" || w.Status == "OVER_QUERY_LIMIT").OrderBy(d => d.UId).ToList();
+                var all = items.Count; bool stopService = false;
                 foreach (var street in items)
                 {
+
                     idx++;
                     var streetLatAndLng = new StreetLatAndLng();
                     streetLatAndLng.UId = street.UId;
@@ -42,40 +143,58 @@ namespace UTConsole
                     streetLatAndLng.Id = street.Id;
                     streetLatAndLng.Tbl = street.Tbl;
                     streetLatAndLng.GoogleApiUrl = street.GoogleApiUrl;
-                    try
+                    streetLatAndLng.Lat = street.Lat;
+                    streetLatAndLng.Lng = street.Lng;
+                    streetLatAndLng.Status = street.Status;
+                    if (streetLatAndLng.Lat == 0.0)
                     {
-
-
-                        var httpClient = new HttpClient();
-                        var response = httpClient.GetAsync(streetLatAndLng.GoogleApiUrl).Result;
-
-                        //will throw an exception if not successful
-                        response.EnsureSuccessStatusCode();
-
-                        string content = response.Content.ReadAsStringAsync().Result;
-                        dynamic o = JObject.Parse(content);
-                        var result = o.results;
-                        if (result != null && result.Count > 0 && result[0] != null && result[0].geometry != null && result[0].geometry.location != null)
+                        if (!stopService)
                         {
-                            var loc = result[0].geometry.location;
+                            try
+                            {
+                                Thread.Sleep(1000);
+                                var httpClient = new HttpClient();
+                                var response = httpClient.GetAsync(streetLatAndLng.GoogleApiUrl).Result;
+                                //will throw an exception if not successful
+                                response.EnsureSuccessStatusCode();
 
-                            streetLatAndLng.Lat = loc.lat;
-                            streetLatAndLng.Lng = loc.lng;
-                            Console.WriteLine("{0} ,{1},City ={2},Addr={3} ", streetLatAndLng.Lat, streetLatAndLng.Lng, streetLatAndLng.City, streetLatAndLng.Addr);
+                                string content = response.Content.ReadAsStringAsync().Result;
+                                dynamic o = JObject.Parse(content);
+                                var result = o.results;
+                                if (result != null && result.Count > 0 && result[0] != null && result[0].geometry != null && result[0].geometry.location != null)
+                                {
+                                    var loc = result[0].geometry.location;
 
+                                    streetLatAndLng.Lat = loc.lat;
+                                    streetLatAndLng.Lng = loc.lng;
+                                    Console.WriteLine("{0} ,{1},City ={2},Addr={3} ", streetLatAndLng.Lat, streetLatAndLng.Lng, streetLatAndLng.City, streetLatAndLng.Addr);
+
+                                }
+                                var status = o.status;
+
+                                streetLatAndLng.Status = status.Value;
+
+                                if (status.Value == "ZERO_RESULTS")
+                                {
+                                    Console.WriteLine("ZERO_RESULTS,City ={0},Addr={1} ", streetLatAndLng.City, streetLatAndLng.Addr);
+                                    continue;
+                                }
+                                if (status.Value == "OVER_QUERY_LIMIT")
+                                {
+                                    Console.WriteLine("OVER_QUERY_LIMIT,City ={0},Addr={1} ", streetLatAndLng.City, streetLatAndLng.Addr);
+                                    stopService = true;
+                                    continue;
+                                }
+
+                            }
+                            catch (Exception ee)
+                            {
+                            }
                         }
-                        var status = o.status;
-                        streetLatAndLng.Status = status.Value;
-
-                        if (status.Value == "ZERO_RESULTS")
-                        {
-                            Console.WriteLine("no found,City ={0},Addr={1} ", streetLatAndLng.City, streetLatAndLng.Addr);
-                            continue;
-                        }
-
                     }
-                    catch (Exception ee)
+                    else
                     {
+                        streetLatAndLng.Status = "Ok";
                     }
                     // streetLatAndLng.Status = "new";
 
@@ -141,6 +260,7 @@ namespace UTConsole
             }
 
         }
+        
         static void FirstLoad()
         {
             StreetsGeoLocation location = new StreetsGeoLocation();
