8a1a580d650bd2f97eba220535cb4f04098f4375
lior grossman <lior_g@g-s.co.il>
2015-08-31 13:57:05 +0300

    f

--- Web/sln/XmlToJsonF4/Program.cs
+++ Web/sln/XmlToJsonF4/Program.cs
@@ -9,6 +9,11 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
+
+using System.Web;
+using System.Xml.Serialization;
+
+
 namespace XmlToJsonF4
 {
     class Program
@@ -122,5 +127,54 @@ namespace XmlToJsonF4
             }
 
         }
+    }
+}
+
+namespace Michal.Project.Models
+{
+    [XmlRoot("ROWDATA")]
+    //  [XmlElement("ROWDATA")]
+    public class Streets
+    {
+        //[XmlArray("ROW")]
+        //[XmlArrayItem("ROW")]
+        [XmlElement("ROW")]
+        public List<Street> StreetsItems { get; set; }
+    }
+
+    //[XmlElement("ROW")]
+    public class Street
+    {
+        [XmlElement("C0")]
+        public string Id { get; set; }
+        [XmlElement("סמל_ישוב")]
+        public string CodeCity { get; set; }
+        [XmlElement("שם_ישוב")]
+        public string City { get; set; }
+
+        [XmlElement("סמל_רחוב")]
+        public string CodeAddr { get; set; }
+        [XmlElement("שם_רחוב")]
+        public string Addr { get; set; }
+        [XmlElement("טבלה")]
+        public string Tbl { get; set; }
+
+    }
+    [JsonObject]
+    public class StreetsGeoLocation
+    {
+
+        public List<StreetLatAndLng> StreetsItems { get; set; }
+    }
+    public class StreetLatAndLng : Street
+    {
+        public string GoogleApiUrl { get; set; }
+        [XmlElement("Lat")]
+        public double Lat { get; set; }
+        [XmlElement("Lng")]
+        public double Lng { get; set; }
+        public string Desc { get; set; }
+        public int UId { get; set; }
+        public string Status { get; set; }//OVER_QUERY_LIMIT,ZERO_RESULTS
     }
 }
