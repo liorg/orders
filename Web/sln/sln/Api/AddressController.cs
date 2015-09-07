5045148cefcbdbf4570712525c70bf6d49f0267c
lior grossman <lior_g@g-s.co.il>
2015-09-07 23:57:08 +0300

    g

---
+++ Web/sln/sln/Api/AddressController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Michal.Project.Api
+{
+    [RoutePrefix("api/Address")]
+    public class AddressController : ApiController
+    {
+        [Route("Cities")]
+        public IHttpActionResult GetCities()
+        {
+            //StreetsGeoLocation location = new StreetsGeoLocation();
+            string path = System.Web.HttpContext.Current.ApplicationInstance.Server.MapPath("~/App_Data/") + "rechovArrange.json";
+               return Ok(new string[] { "value1", path });
+        }
+
+    }
+}
--- Web/sln/sln/Models/Streets.cs
+++ Web/sln/sln/Models/Streets.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,15 +7,46 @@ using System.Xml.Serialization;
 
 namespace Michal.Project.Models
 {
+    [XmlRoot("ROWDATA")]
+     public class Streets
+    {
+     
+        [XmlElement("ROW")]
+        public List<Street> StreetsItems { get; set; }
+    }
 
-    //[XmlElement("ROWDATA")]
-    //public class Streets
-    //{
+    public class Street
+    {
+        [XmlElement("C0")]
+        public string Id { get; set; }
+        [XmlElement("סמל_ישוב")]
+        public string CodeCity { get; set; }
+        [XmlElement("שם_ישוב")]
+        public string City { get; set; }
 
-    //}
+        [XmlElement("סמל_רחוב")]
+        public string CodeAddr { get; set; }
+        [XmlElement("שם_רחוב")]
+        public string Addr { get; set; }
+        [XmlElement("טבלה")]
+        public string Tbl { get; set; }
 
+    }
+    [JsonObject]
+    public class StreetsGeoLocation
+    {
 
-    //public class Street
-    //{
-    //}
+        public List<StreetLatAndLng> StreetsItems { get; set; }
+    }
+    public class StreetLatAndLng : Street
+    {
+        public string GoogleApiUrl { get; set; }
+        [XmlElement("Lat")]
+        public double Lat { get; set; }
+        [XmlElement("Lng")]
+        public double Lng { get; set; }
+        public string Desc { get; set; }
+        public int UId { get; set; }
+        public string Status { get; set; }//OVER_QUERY_LIMIT,ZERO_RESULTS
+    }
 }
