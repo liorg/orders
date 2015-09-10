fdbc8022686b36fd035b365584b962ec7b058c9d
lior grossman <lior_g@g-s.co.il>
2015-09-10 07:17:48 +0300

    d

--- Web/sln/sln/Scripts/AutocmpleteCascade.js
+++ Web/sln/sln/Scripts/AutocmpleteCascade.js
@@ -1,29 +1,76 @@
-﻿(function ($) {
+﻿
+$(window).load(
+       function () {
+
+           //$.ui.autocomplete.prototype.options.autoSelect = true; 
+           // Doesn't appear to work in ui 1.10.3
+           // http://github.com/scottgonzalez/jquery-ui-extensions
+
+           $(".ui-autocomplete-input").bind('autocompleteresponse',
+                   function (event, ui) {
+                       $(this).data('menuItems', ui.content);
+                   });
+
+           $(".ui-autocomplete-input").on(
+                   "blur",
+                   null,
+                   function (event) {
+                       var autocomplete = $(this).data("uiAutocomplete");
+                       if (!autocomplete.options.autoSelect
+                               || autocomplete.selectedItem) {
+                           return;
+                       }
+
+                       var matcher = new RegExp("^"
+                               + $.ui.autocomplete.escapeRegex($(this).val())
+                               + "$", "i");
+                       var menuItems = $(this).data('menuItems');
+                       for (idx in menuItems) {
+                           var item = menuItems[idx];
+                           if (matcher.test(item.value)) {
+                               autocomplete.selectedItem = item;
+                               break;
+                               // return false;
+                           }
+                       }
+                       if (autocomplete.selectedItem) {
+                           autocomplete._trigger("select", event, {
+                               item: autocomplete.selectedItem
+                           });
+                       } else {
+                           this.value = '';
+                       }
+                   });
+
+       });
+
+
+(function ($) {
 
     function activateChild() {
         var child = $(this);
-        child
-            .prop("disabled", false)
-            .removeClass("disabled");
+        child.prop("disabled", false).removeClass("disabled");
         if (child.autocomplete) {
             child.autocomplete("enable");
         }
+        var childval = $("#" + child[0].id + "Code");
+        childval.prop("disabled", false).removeClass("disabled");
     }
 
     function deactivateChild() {
         var child = $(this);
-        child
-            .prop("disabled", true)
-            .val("")
-            .addClass("disabled");
+        child.prop("disabled", true).val("").addClass("disabled");
         if (child.autocomplete) {
             child.autocomplete("disable");
         }
-
+        var childval = $("#" + child[0].id + "Code");
+        childval.prop("disabled", true).val("").addClass("disabled");
+        
         var subChild = child.data("cascade-child");
         if (subChild) {
             deactivateChild.call(subChild);
         }
+       
     }
 
     function eventOverride(originalEvent) {
