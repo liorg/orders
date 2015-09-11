47dc1061d0195b00539fe5671978429c07aae724
lior grossman <lior_g@g-s.co.il>
2015-09-11 16:42:28 +0300

    assad

--- Web/sln/sln/Scripts/AutocmpleteCascade.js
+++ Web/sln/sln/Scripts/AutocmpleteCascade.js
@@ -1,116 +1,101 @@
-﻿
-//$(window).load(
-//       function () {
+﻿$(document).ready(function () {
+    debugger;
+    $('.autp').each(initialize);
+});
+var initialize = function (i, el) {
+    debugger;
+    // el is the input element that we need to initialize a map for, jQuery-ize it,
+    //  and cache that since we'll be using it a few times.
+    var $input = $(el);
+    var city = "#" + $input.val() + "city";
+    var street = "#" + $input.val() + "street";
+    var citycode = city + "code";
+    var streetcode = street + "code";
 
-//           //$.ui.autocomplete.prototype.options.autoSelect = true; 
-//           // Doesn't appear to work in ui 1.10.3
-//           // http://github.com/scottgonzalez/jquery-ui-extensions
+    $(city).bind('autocompleteresponse',
+         function (event, ui) {
+             $(city).data('menuItems', ui.content);
+         }).blur(function () {
+             debugger;
+             var autocomplete = $(city).data("uiAutocomplete");
+             var matcher = new RegExp("^"
+                     + $.ui.autocomplete.escapeRegex($(this).val())
+                     + "$", "i");
+             var menuItems = $(this).data('menuItems');
+             for (idx in menuItems) {
+                 var item = menuItems[idx];
+                 if (matcher.test(item.value)) {
+                     autocomplete.selectedItem = item;
+                     break;
+                 }
+             }
+             if (autocomplete.selectedItem) {
+                 autocomplete._trigger("select", event, {
+                     item: autocomplete.selectedItem
+                 });
+             } else {
+                 $(streetcode).val("");
+                 $(street).val("");
+                 $(citycode).val("");
+                 $(city).val("");
+             }
+         }).autocomplete({
+             source: "/api/address/SearchCities",
+             html: true, autoFocus: true, selectFirst: true, autoSelect: true,
+             minLength: 2,
+             open: function (event, ui) {
+                 $(".ui-autocomplete").css("z-index", 1000);
+             },
+             select: function (event, ui) {
+                 debugger;
+                 log(ui.item ? "Selected: " + ui.item.value + " aka " + ui.item.id : "Nothing selected, input was " + this.value);
+                 $(citycode).val(ui.item.id);
 
-//           $(".ui-autocomplete-input").bind('autocompleteresponse',
-//                   function (event, ui) {
-//                       $(this).data('menuItems', ui.content);
-//                   });
-
-//           $(".ui-autocomplete-input").on(
-//                   "blur",
-//                   null,
-//                   function (event) {
-//                       var autocomplete = $(this).data("uiAutocomplete");
-//                       if (!autocomplete.options.autoSelect
-//                               || autocomplete.selectedItem) {
-//                           return;
-//                       }
-
-//                       var matcher = new RegExp("^"
-//                               + $.ui.autocomplete.escapeRegex($(this).val())
-//                               + "$", "i");
-//                       var menuItems = $(this).data('menuItems');
-//                       for (idx in menuItems) {
-//                           var item = menuItems[idx];
-//                           if (matcher.test(item.value)) {
-//                               autocomplete.selectedItem = item;
-//                               break;
-//                               // return false;
-//                           }
-//                       }
-//                       if (autocomplete.selectedItem) {
-//                           autocomplete._trigger("select", event, {
-//                               item: autocomplete.selectedItem
-//                           });
-//                       } else {
-//                           this.value = '';
-//                         //  alert(this.id);
-//                       }
-//                   });
-
-//       });
+                 $(streetcode).val("")
+                 $(street).val("");//.prop("disabled", false);
+             }
 
 
-//(function ($) {
+         });
+    $(street).autocomplete({
+        html: true, autoFocus: true, selectFirst: true, autoSelect: true,
+        open: function (event, ui) {
+            $(".ui-autocomplete").css("z-index", 1000);
+        },
+        source: function (request, response) {
+            var pfId = $(citycode).val();
+            if (pfId == "") {
+                $(street).removeClass("ui-autocomplete-loading");
+                alert("יש לבחור עיר");
 
-//    function activateChild() {
-//        var child = $(this);
-//        child.prop("disabled", false).removeClass("disabled");
-//        if (child.autocomplete) {
-//            child.autocomplete("enable");
-//        }
-//        var childval = $("#" + child[0].id + "Code");
-//        childval.prop("disabled", false).removeClass("disabled");
-//    }
+                request.preventDefault();
+                return false;
+            }
+            $(street).addClass("ui - autocomplete - loading");
+            $.ajax({
+                url: "/api/address/SearchStreets",
+                dataType: "json",
+                type: 'GET',
+                data: {
+                    MaxItems: 12,
+                    ParentFilterId: pfId,
+                    Term: request.term
+                },
+                success: function (data) {
+                    $(street).removeClass("ui-autocomplete-loading");
+                    response(data);
+                }
+            });
+        },
+        minLength: 2,
+        select: function (event, ui) {
+            $(streetcode).val(ui.item.id);
+            log(ui.item ? "Selected: " + ui.item.value + " aka " + ui.item.id : "Nothing selected, input was " + this.value);
+        },
+    });
 
-//    function deactivateChild() {
-//        var child = $(this);
-//        child.prop("disabled", true).val("").addClass("disabled");
-//        if (child.autocomplete) {
-//            child.autocomplete("disable");
-//        }
-//        var childval = $("#" + child[0].id + "Code");
-//        childval.prop("disabled", true).val("").addClass("disabled");
-        
-//        var subChild = child.data("cascade-child");
-//        if (subChild) {
-//            deactivateChild.call(subChild);
-//        }
-       
-//    }
-
-//    function eventOverride(originalEvent) {
-//        var child = this;
-//        return function (event, ui) {
-//            var isChildActivate = child.prop("disabled") === false;
-//            if (!ui.item && isChildActivate) {
-//                deactivateChild.call(child);
-//            } else if (ui.item && !isChildActivate) {
-//                activateChild.call(child);
-//            }
-//            if (typeof originalEvent === "function") {
-//                originalEvent.call(child, event, ui);
-//            }
-//        };
-//    }
-
-//    $.fn.cascade = function (child) {
-//        var parent = this;
-//        child = $(child);
-
-//        if (parent.autocomplete) {
-//            var originalChange = parent.autocomplete("option", "change");
-//            var originalSelect = parent.autocomplete("option", "select");
-//            parent.data("cascade-child", child);
-//            parent.autocomplete("option", "change", eventOverride.call(child, originalChange));
-//            parent.autocomplete("option", "select", eventOverride.call(child, originalSelect));
-//        }
-
-//        deactivateChild.call(child);
-
-//        return parent;
-//    };
-
-//    $.cascadingAutocompletes = function (autocompletes) {
-//        for (var i = 0; i < autocompletes.length - 1; i++) {
-//            if (autocompletes[i]) {
-//                $(autocompletes[i]).cascade(autocompletes[i + 1]);
-//            }
-//        }
-//    };
-//})(jQuery);
+}
+function log(message) {
+    $("<div>").text(message).prependTo("#log");
+    $("#log").scrollTop(0);
+}
