2700c612f9ff2a3a32e4ae1771c393e7bbefacec
lior grossman <lior_g@g-s.co.il>
2015-09-21 17:24:32 +0300

    s

---
+++ Web/sln/sln/Controllers/ErrorPageController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
+using Michal.Project.Models;
+using Michal.Project.Helper;
+using Michal.Project.Dal;
+using Michal.Project.DataModel;
+using System.Data.Entity.Validation;
+using System.Data.Entity;
+using Michal.Project.Bll;
+using Michal.Project.Models.View;
+
+namespace Michal.Project.Controllers
+{
+    public class ErrorPageController : Controller
+    {
+        public ActionResult Oops(int id)
+        {
+            Response.StatusCode = id;
+            return View();
+        }
+    }
+}
