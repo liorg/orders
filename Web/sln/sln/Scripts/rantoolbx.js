0c80194d0b01a243595be59f0a08a457508b9607
lior grossman <lior_g@g-s.co.il>
2015-10-02 05:59:23 +0300

    s

--- Web/sln/sln/Scripts/rantoolbx.js
+++ Web/sln/sln/Scripts/rantoolbx.js
@@ -23,4 +23,35 @@
         //  alert(url);
         changeUrl(url);
     });
+    $("#btnEdit").click(function (e) {
+        //debugger;
+        e.preventDefault();
+        if ($("#frm").valid()) {
+            //submit
+            $("#frm").submit();
+        }
+        else
+            return false;
+    });
+    $('#btnBack').click(function () {
+        var url = $(this).attr("data-url");
+        changeUrl(url);
+    });
+    $('#btnCancel').click(function () {
+        var url = $(this).attr("data-url");
+        changeUrl(url);
+    });
+    $('#btnNext2').click(function () {
+        var url = $(this).attr("data-url");
+        changeUrl(url);
+    });
+    $('#btnNext3').click(function () {
+        var url = $(this).attr("data-url");
+        changeUrl(url);
+    });
+    $('#btnAddItem').click(function () {
+        var url = $(this).attr("data-url");
+        changeUrl(url);
+    });
+
 });
