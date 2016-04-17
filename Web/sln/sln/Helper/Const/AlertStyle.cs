f30224f3434e555861e4f0cf592e555f1faedafc
lior grossman <lior.grosman@gmail.com>
2016-04-17 17:43:10 +0300

    OK

---
+++ Web/sln/sln/Helper/Const/AlertStyle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Michal.Project.Helper
+{
+    public enum AlertStyle
+    {
+        Default = 0,
+        Success = 4,
+        Warning = 3,
+        Info = 1,
+        Error = 2,
+        WaitingGet = 5,//timewait on get delievry
+        WaitingSet = 6//timewait on set delievry
+    }
+
+}
